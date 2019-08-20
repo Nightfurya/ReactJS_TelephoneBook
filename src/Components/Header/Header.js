498ab2bb64aa589db8c9e6a79c6304609a90a377
Nightfurya <y.m.logvinenko@gmail.com>
2019-08-20 21:25:32 +0300

    component header - done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./Header.scss";
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className="link__container">
+          <a href="https://create-react-app.dev/" target="_blank" className="link link__header">
+            React
+          </a>
+        </div>
+        <div className="paragraph__container">
+          <p className="paragraph__header">Telephone Book</p>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
